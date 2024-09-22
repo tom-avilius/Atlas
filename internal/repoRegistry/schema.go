42ed5ef209df93c83f8d1e140234d1db647b1eff
tom avilius <tomavilius@tomavilius.in>
2024-09-23 01:31:12 +0530

    Define the data structures to be used by repoRegistry.

---
+++ internal/repoRegistry/schema.go
@@ -0,0 +1,40 @@
+
+// Package repoRegistry provides data structures and interfaces for managing repositories.
+// This module allows for adding, removing, and syncing repository information.
+package reporegistry
+
+
+import "time"
+
+
+// Repository represents a git repository with its associated metadata.
+// It includes the repository's name, URL, and timestamps for when it was added
+// and last synced.
+type Repository struct {
+  
+  Name     string    // Name of the repository
+  Url      string    // URL of the repository
+  AddedAt  time.Time // Timestamp when the repository was added
+  LastSync time.Time // Timestamp of the last sync operation
+}
+
+// Credentials holds user authentication details for accessing a repository.
+// It includes the username and password required for access.
+type Credentials struct {
+  
+  Username string // Username for accessing the repository
+  Password string // Password for accessing the repository
+}
+
+// RepositoryManager defines methods for managing repositories.
+// It includes methods to add and remove repositories.
+type RepositoryManager interface {
+
+  // TODO: Instead of using the standard error interface, use an advanced and modified version from a local module instead.
+
+  // Add adds a repository with the specified credentials.
+  Add(repo Repository, cred Credentials) error
+  // Remove removes a repository by its name.
+  Remove(repositoryName string) error
+}
+
