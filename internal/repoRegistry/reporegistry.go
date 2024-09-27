e7d1f56b46e1555ad34c5d2bf0e8b5ac5acc4b6a
tom avilius <tomavilius@tomavilius.in>
2024-09-28 00:52:57 +0530

    Add function to handle adding of repository.

---
+++ internal/repoRegistry/reporegistry.go
@@ -0,0 +1,27 @@
+
+package reporegistry
+
+func AddRepository (repo Repository, clonePath string) bool {
+
+  // checking if the config file exists
+  if !checkFileExist(configFilePath) {
+
+    // creating the file when it does not exist
+    createDir()
+    createFile(configFilePath)
+  }
+
+  // validating the url.
+  isValidUrl := validateUrl(repo.Url);
+
+  if isValidUrl {
+
+    if cloneRepo(repo.Url, clonePath) {
+
+      return writeYaml(configFilePath, repo)
+    }
+  }
+
+  return false
+}
+
