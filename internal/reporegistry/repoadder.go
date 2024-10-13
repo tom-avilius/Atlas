ab2b7109bdcbeb3afc2296f7a7fa30fa43e54047
tom avilius <tomavilius@tomavilius.in>
2024-10-13 13:02:59 +0530

    Document repoadder.go

--- internal/reporegistry/repoadder.go
+++ internal/reporegistry/repoadder.go
@@ -1,6 +1,7 @@
 
 package reporegistry
 
+
 import (
 	
   "fmt"
@@ -12,9 +13,15 @@ import (
 )
 
 
+
+// validateUrl checks if the url is valid.
+// returns false if the url is not valid or an error occurs.
+// url should be of a github repository.
 func validateUrl (repoUrl string) bool {
 
+  // parsing the url
   parsedUrl, err := url.ParseRequestURI(repoUrl);
+  // Log if an error occurs
   if err != nil {
 
     fmt.Println("Error occured while parsing url.")
@@ -22,35 +29,43 @@ func validateUrl (repoUrl string) bool {
     return false
   }
 
+  // check for its validity
   if (parsedUrl.Scheme == "" || parsedUrl.Host == "") {
 
     return false
   }
 
+  // check for Host
   if !strings.Contains(parsedUrl.Host, ".") {
 
     fmt.Println("Invalid Url Provided.")
     return false
   }
 
+  // check for github url
   if !strings.HasPrefix(repoUrl, "https://github.com") {
 
     fmt.Println("Error: Not a github link.")
     return false 
   }
   
+  // safely exist the function.
   return true
 }
 
 
+// cloneRepo clones the given repository
+// return false if an error occurs
 func cloneRepo (repoUrl string, clonePath string) bool {
 
+  // performing plain clone of the repository.
   _, err := git.PlainClone(clonePath, false, &git.CloneOptions{
 
     URL: repoUrl,
     Progress: os.Stdout,
   })
 
+  // Log if an error occurs. 
   if err != nil {
 
     fmt.Println("Error occured while cloning the repository.")
@@ -58,6 +73,7 @@ func cloneRepo (repoUrl string, clonePath string) bool {
     return false  
   }
 
+  // safely exit the function
   return true
 }
 
