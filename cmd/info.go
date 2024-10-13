1f6bd8da2f0bec3d0bb9e2635ab87632ca243084
tom avilius <tomavilius@tomavilius.in>
2024-10-13 21:14:37 +0530

    Add basic command info to list in detail the complete atlas config.

---
+++ cmd/info.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"tomavilius.in/atlas/internal/repoinformer"
+)
+
+
+var infoCommand =  &cobra.Command {
+
+  Use: "info",
+  Short: "Detailed list view of atlas config.",
+  Long: "Lists in detail the config file of atlas.",
+  Run: func(cmd *cobra.Command, args []string) {
+
+    repoinformer.DetailedRepositoryView()
+  },
+}
