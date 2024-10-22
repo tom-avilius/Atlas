563d77da5046cb448eb71e3bffc60742451c64b0
tom-avilius <tomavilius@tomavilius.in>
2024-10-22 22:30:56 +0530

    Move handleHomeDirectory to reporegistry.go

--- internal/reporegistry/filemanager.go
+++ internal/reporegistry/filemanager.go
@@ -5,43 +5,18 @@ package reporegistry
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"gopkg.in/yaml.v3"
 )
 
 
 
-// function the fills in the home directory when ~ is provided at the start
-func handleHomeDirectory (filePath string) (string, bool) {
-
- if strings.HasPrefix(filePath, "~") {
-    
-    // getting the home directory
-    homeDir, err := os.UserHomeDir()
-
-    // Log if an error occurs
-    if err != nil {
-
-      fmt.Println("Error while getting the home directory.")
-      fmt.Println("Error Log:")
-      fmt.Println(err)
-      return "", false
-    }
-
-    // otherwise concatenate home dir with the file path.
-    filePath = strings.Join([]string{homeDir}, filePath[1:])
-  }
-
-  return filePath, true
-}
-
 // checkFileExist checks whether a file exists or not.
 // returns false when it can not find the file or any error occurs.
 func checkFileExist (filePath string) bool {
 
   // resolving for home dir
-  if path, success := handleHomeDirectory(filePath); success {
+  if path, success := HandleHomeDirectory(filePath); success {
 
     filePath = path
   } else {
@@ -92,11 +67,12 @@ func createDir (dir string) bool {
 func createFile (filepath string) bool {
 
   // handling ~ paths
-  if path, success := handleHomeDirectory(filepath); success {
+  if path, success := HandleHomeDirectory(filepath); success {
 
     filepath = path
   } else {
-
+    
+    // unsuccessful 
     return false
   }
 
