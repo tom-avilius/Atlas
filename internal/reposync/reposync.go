a4fc935185355aea5073420d4d1f97655b1d75f6
tom-avilius <tomavilius@tomavilius.in>
2024-10-24 23:30:00 +0530

    Document reposync.go.

--- internal/reposync/reposync.go
+++ internal/reposync/reposync.go
@@ -7,14 +7,16 @@ import (
 	"tomavilius.in/atlas/internal/reporegistry"
 )
 
-// function to write path data
+// function to write path data to path file
 func WritePathData() bool {
 
+  // creating path file if it does not exist
   if success := createPathFile(); !success {
 
     return false
   }
 
+  // reading the config file
   fmt.Println("\nReading Path file..")
   config, success := repoinformer.ReadYaml(reporegistry.ConfigFilePath);
   if !success {
@@ -24,6 +26,7 @@ func WritePathData() bool {
   }
   fmt.Println("Done")
 
+  // looping through the config data to extract path info 
   for _, data := range config.Repositories {    
 
     dirData, success := dirList(data.Path)
@@ -33,9 +36,11 @@ func WritePathData() bool {
       return false
     }
 
+    // to store path data
     var dirDataFormat reporegistry.PathData
     dirDataFormat.Paths = dirData
 
+    // writing path data to the path file
     fmt.Println("\nWriting to path file..")
     if !writePathData(reporegistry.PathFilePath, dirDataFormat) {
 
@@ -44,6 +49,6 @@ func WritePathData() bool {
     fmt.Println("Done")
   }
 
-  fmt.Println("\nSyncing Completed.")
+  // successful execution
   return true
 }
