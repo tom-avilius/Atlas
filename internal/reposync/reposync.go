c2a02893ac0e0f11db394083a9dfba4329d979b9
tom-avilius <tomavilius@tomavilius.in>
2024-10-24 22:02:57 +0530

    Add function to write path data topath data file.

--- internal/reposync/reposync.go
+++ internal/reposync/reposync.go
@@ -1,7 +1,45 @@
-
 package reposync
 
+import (
+	"fmt"
 
+	"tomavilius.in/atlas/internal/repoinformer"
+	"tomavilius.in/atlas/internal/reporegistry"
+)
 
 // function to write path data
-func WritePathData() bool {}
+func WritePathData() bool {
+
+  if success := createPathFile(); !success {
+
+    return false
+  }
+
+  config, success := repoinformer.ReadYaml(reporegistry.ConfigFilePath);
+
+  if !success {
+
+    fmt.Println("Error while reading config file.")
+    return false
+  }
+
+  for _, data := range config.Repositories {    
+
+    dirData, success := dirList(data.Path)
+
+    if !success {
+
+      return false
+    }
+
+    var dirDataFormat reporegistry.PathData
+    dirDataFormat.Paths = dirData
+
+    if !writePathData(reporegistry.PathFilePath, dirDataFormat) {
+
+      return false
+    }
+  }
+
+  return true
+}
