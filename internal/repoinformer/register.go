3584343c6504deecc398ecbed6e7fac29da23f56
tom avilius <tomavilius@tomavilius.in>
2024-12-15 13:43:20 +0530

    Use more comprehensive error logging for info and list command.

--- internal/repoinformer/register.go
+++ internal/repoinformer/register.go
@@ -1,6 +1,4 @@
-
 package repoinformer
-
 
 import (
 	"fmt"
@@ -9,53 +7,52 @@ import (
 	"tomavilius.in/atlas/internal/reporegistry"
 )
 
-
-
 // listRepositories lists the complete atlas config after formatting it.
-func listRepositories () bool {
+func listRepositories() bool {
 
-  // read the config file
-  data, success := ReadYaml(reporegistry.ConfigFilePath)
+	// read the config file
+	data, success := ReadYaml(reporegistry.ConfigFilePath)
 
-  // if read yaml was unsuccessful then simply return false
-  if !success {
+	// if read yaml was unsuccessful then simply return false
+	if !success {
 
-    return false
-  }
+		fmt.Println("Could not read atlas config file. Maybe you have not provided any repository for atlas to watch yet.")
+		return false
+	}
 
-  // format the config to display
-  for i := 0; i < len(data.Repositories); i++ {
+	// format the config to display
+	for i := 0; i < len(data.Repositories); i++ {
 
-    fmt.Println()
-    fmt.Println("Name: \t\t" +data.Repositories[i].Name)
-    fmt.Println("Url: \t\t" +data.Repositories[i].Url)
-    fmt.Println("Path: \t\t" +data.Repositories[i].Path)
-    fmt.Println("Last Sync: \t" +data.Repositories[i].LastSync.Format(time.RFC850))
-    fmt.Println("Added At: \t" +data.Repositories[i].AddedAt.Format(time.RFC850))
-  }
+		fmt.Println()
+		fmt.Println("Name: \t\t" + data.Repositories[i].Name)
+		fmt.Println("Url: \t\t" + data.Repositories[i].Url)
+		fmt.Println("Path: \t\t" + data.Repositories[i].Path)
+		fmt.Println("Last Sync: \t" + data.Repositories[i].LastSync.Format(time.RFC850))
+		fmt.Println("Added At: \t" + data.Repositories[i].AddedAt.Format(time.RFC850))
+	}
 
-  return success;
+	return success
 }
 
-
 // to only list Repositories from atlas config.
-func listOnlyRepositories () bool {
-  
-  // read the config file
-  data, success := ReadYaml(reporegistry.ConfigFilePath)
+func listOnlyRepositories() bool {
 
-  // if read yaml was unsuccessful then simply return false
-  if !success {
+	// read the config file
+	data, success := ReadYaml(reporegistry.ConfigFilePath)
 
-    return false
-  }
+	// if read yaml was unsuccessful then simply return false
+	if !success {
 
-  // display Repositories
-  fmt.Println()
-  for i := 0; i < len(data.Repositories); i++ {
+		fmt.Println("Could not read atlas config file. Maybe you have not added any repository yet for atlas to watch.")
+		return false
+	}
 
-    fmt.Println(data.Repositories[i].Name)
-  }
+	// display Repositories
+	fmt.Println()
+	for i := 0; i < len(data.Repositories); i++ {
 
-  return success;
+		fmt.Println(data.Repositories[i].Name)
+	}
+
+	return success
 }
