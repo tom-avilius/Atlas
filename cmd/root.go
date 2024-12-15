4b9c53c03f6070a024f56e31248fe9cc29655ab8
tom avilius <tomavilius@tomavilius.in>
2024-12-15 13:32:23 +0530

    Add install command documentation at the root command.

--- cmd/root.go
+++ cmd/root.go
@@ -7,35 +7,35 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
 var rootCommand = &cobra.Command{
 
-  Use: "atlas",
-  Short: "A backup tool for you configs and notes.",
-  Long: `Atlas uses git to backup the files and folders.
+	Use:   "atlas",
+	Short: "A backup tool for you configs and notes.",
+	Long: `Atlas uses git to backup the files and folders.
 You must first create a git repository and provide the url for atlas to do its thing.`,
-  Run: func(cmd *cobra.Command, args []string) {
-    
-    fmt.Println("Usage atlas [command]")
+	Run: func(cmd *cobra.Command, args []string) {
 
-    fmt.Println("\nAvailable Commands:")
-    fmt.Println("version \t Display the atlas project version.")
-    fmt.Println("add \t Add a git repository for atlas to make backups to.")
-    fmt.Println("remove \t Remove a git repository for atlas to stop making backups to.")
-    fmt.Println("sync \t Explicitly sync all the repositories managed by atlas.")
-    fmt.Println("watch \t Re-evaluate path file and attach fsnotify to them.")
-    fmt.Println("list \t Lists all the repositories managed by atlas.")
-    fmt.Println("info \t Detailed list view of atlas config.")
-  },
+		fmt.Println("Usage atlas [command]")
+
+		fmt.Println("\nAvailable Commands:")
+		fmt.Println("version \t Display the atlas project version.")
+		fmt.Println("add \t Add a git repository for atlas to make backups to.")
+		fmt.Println("remove \t Remove a git repository for atlas to stop making backups to.")
+		fmt.Println("sync \t Explicitly sync all the repositories managed by atlas.")
+		fmt.Println("watch \t Re-evaluate path file and attach fsnotify to them.")
+		fmt.Println("list \t Lists all the repositories managed by atlas.")
+		fmt.Println("info \t Detailed list view of atlas config.")
+		fmt.Println("install \t Run the necessary install scripts.")
+	},
 }
 
-func Execute () {
+func Execute() {
 
-  err := rootCommand.Execute()
+	err := rootCommand.Execute()
 
-  if err != nil {
+	if err != nil {
 
-    fmt.Println(err)
-    os.Exit(1)
-  }
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
