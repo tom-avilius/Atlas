cb4e12116d1a4a4008e6c0029e1f80f7ef36e66b
tom avilius <tomavilius@tomavilius.in>
2024-12-18 00:10:32 +0530

    Add comments to info.go.

--- cmd/info.go
+++ cmd/info.go
@@ -1,3 +1,14 @@
+/**
+*
+* @file: info.go
+* @description: This file contains the add command that adds a repository for atlas to watch and make backups to.
+*
+* @author: tom avilius <tomavilius@tomavilius.in>
+* @license: MIT license
+* @package: Atlas v0.0.1 development
+*
+**/
+
 package cmd
 
 import (
@@ -5,19 +16,21 @@ import (
 	"tomavilius.in/atlas/internal/repoinformer"
 )
 
+// add info command to the root command.
+func init() {
 
-func init () {
-
-  rootCommand.AddCommand(infoCommand)
+	rootCommand.AddCommand(infoCommand)
 }
 
-var infoCommand =  &cobra.Command {
+// info command to give detailed view of atlas config. Prints everything.
+var infoCommand = &cobra.Command{
 
-  Use: "info",
-  Short: "Detailed list view of atlas config.",
-  Long: "Lists in detail the config file of atlas.",
-  Run: func(cmd *cobra.Command, args []string) {
+	Use:   "info",
+	Short: "Detailed list view of atlas config.",
+	Long:  "Lists in detail the config file of atlas.",
+	Run: func(cmd *cobra.Command, args []string) {
 
-    repoinformer.DetailedRepositoryView()
-  },
+		// transfer control to DetailedRepositoryView function to print entire atlas config.
+		repoinformer.DetailedRepositoryView()
+	},
 }
