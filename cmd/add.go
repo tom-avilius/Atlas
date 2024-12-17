1aba0ac391cf39cb1fea46b06e48d2ca79f73bbe
tom avilius <tomavilius@tomavilius.in>
2024-12-17 23:50:51 +0530

    Add file doc to root.go.

--- cmd/add.go
+++ cmd/add.go
@@ -1,3 +1,14 @@
+/**
+*
+* @file: add.go
+* @description: This file contains the add command that adds a repository for atlas to watch and make backups to.
+*
+* @author: tom avilius <tomavilius@tomavilius.in>
+* @license: MIT license
+* @package: Atlas v0.0.1 development
+*
+**/
+
 package cmd
 
 import (
@@ -13,82 +24,81 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func init() {
 
-func init () {
-
-  rootCommand.AddCommand(addCommand)
+	rootCommand.AddCommand(addCommand)
 }
 
-var addCommand = &cobra.Command {
-  
-  Use: "add",
-  Short: "Add a git repository",
-  Long: "Add a git repository for atlas to manage and make backups to.",
-  Run: func(cmd *cobra.Command, args []string) {
+var addCommand = &cobra.Command{
 
-    fmt.Println("\nThis command will walk you over adding a new git repository to atlas.")
-    fmt.Println("The specified local folder or file would be backed up to this repository.")
+	Use:   "add",
+	Short: "Add a git repository",
+	Long:  "Add a git repository for atlas to manage and make backups to.",
+	Run: func(cmd *cobra.Command, args []string) {
 
-    reader := bufio.NewReader(os.Stdin)
+		fmt.Println("\nThis command will walk you over adding a new git repository to atlas.")
+		fmt.Println("The specified local folder or file would be backed up to this repository.")
 
-    fmt.Print("\nName: ")
-    repoName, _ := reader.ReadString('\n');
-    repoName = strings.ToLower(repoName)
+		reader := bufio.NewReader(os.Stdin)
 
-    fmt.Print("Url: ")
-    repoUrl, _ := reader.ReadString('\n')
+		fmt.Print("\nName: ")
+		repoName, _ := reader.ReadString('\n')
+		repoName = strings.ToLower(repoName)
 
-    var clonePath string
-    homeDir, err := os.UserHomeDir()
-    if err == nil {
+		fmt.Print("Url: ")
+		repoUrl, _ := reader.ReadString('\n')
 
-      fmt.Print("Clone Path: " +homeDir);
-      clonePath, _ = reader.ReadString('\n')
-    } else {
+		var clonePath string
+		homeDir, err := os.UserHomeDir()
+		if err == nil {
 
-      fmt.Println("Error occured while getting home directory.")
-      fmt.Println("Error Log:")
-      fmt.Println(err);
-      os.Exit(1)
-    }
+			fmt.Print("Clone Path: " + homeDir)
+			clonePath, _ = reader.ReadString('\n')
+		} else {
 
-    repoName = repoName[:len(repoName)-1]
-    repoUrl = repoUrl[:len(repoUrl)-1]
-    clonePath = clonePath[:len(clonePath)-1]
-    clonePath = homeDir+clonePath
+			fmt.Println("Error occured while getting home directory.")
+			fmt.Println("Error Log:")
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
-    fmt.Println("\nConfirm Action:")
-    fmt.Println("Name:\t" +repoName + "\t(need not be the github repo name)")
-    fmt.Println("Url:\t" +repoUrl + "\t(needs to be the github repo link)")
-    fmt.Println("Path:\t" +clonePath +"\t(the local clone path)")
-    
-    fmt.Println("\nConfirm Action by pressing y or n to abort.")
-    choice, _ := reader.ReadString('\n');
+		repoName = repoName[:len(repoName)-1]
+		repoUrl = repoUrl[:len(repoUrl)-1]
+		clonePath = clonePath[:len(clonePath)-1]
+		clonePath = homeDir + clonePath
 
-    if len(choice) > 2 {
+		fmt.Println("\nConfirm Action:")
+		fmt.Println("Name:\t" + repoName + "\t(need not be the github repo name)")
+		fmt.Println("Url:\t" + repoUrl + "\t(needs to be the github repo link)")
+		fmt.Println("Path:\t" + clonePath + "\t(the local clone path)")
 
-      fmt.Println("Too long an input. \nAborting..")
-      os.Exit(1)
-    }
+		fmt.Println("\nConfirm Action by pressing y or n to abort.")
+		choice, _ := reader.ReadString('\n')
 
-    if choice[0] == 'y' || choice[0] == 'Y' {
-      
-      repo := reporegistry.Repository {
-        
-        Name: strings.TrimSpace(repoName),
-        Url: repoUrl,
-        Path: path.Join(clonePath, repoName),
-        AddedAt: time.Now(),
-        LastSync: time.Time{},
-      } 
+		if len(choice) > 2 {
 
-      reporegistry.AddRepository(repo, clonePath)
-    } else if choice[0] == 'n' || choice[0] == 'N' {
+			fmt.Println("Too long an input. \nAborting..")
+			os.Exit(1)
+		}
 
-      fmt.Println("\nAborting.. \nNo changes have been made.")
-    } else {
+		if choice[0] == 'y' || choice[0] == 'Y' {
 
-      fmt.Println("\nNo option selected. \nAborting..")
-    }
-  },
+			repo := reporegistry.Repository{
+
+				Name:     strings.TrimSpace(repoName),
+				Url:      repoUrl,
+				Path:     path.Join(clonePath, repoName),
+				AddedAt:  time.Now(),
+				LastSync: time.Time{},
+			}
+
+			reporegistry.AddRepository(repo, clonePath)
+		} else if choice[0] == 'n' || choice[0] == 'N' {
+
+			fmt.Println("\nAborting.. \nNo changes have been made.")
+		} else {
+
+			fmt.Println("\nNo option selected. \nAborting..")
+		}
+	},
 }
