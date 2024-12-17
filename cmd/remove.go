3b103593a41aabfa43f52e01a8884407979ee6f7
tom avilius <tomavilius@tomavilius.in>
2024-12-18 00:32:23 +0530

    Add comments to remove.go.

--- cmd/remove.go
+++ cmd/remove.go
@@ -1,3 +1,14 @@
+/**
+*
+* @file: add.go
+* @description: This file contains the remove command to remove a repository from config. Opposite to add command.
+*
+* @author: tom avilius <tomavilius@tomavilius.in>
+* @license: MIT license
+* @package: Atlas v0.0.1 development
+*
+**/
+
 package cmd
 
 import (
@@ -11,43 +22,47 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func init() {
 
-func init () {
+	rootCommand.AddCommand(removeCommand)
 
-  rootCommand.AddCommand(removeCommand)
-
-  // --clear | -c | To clear the entire config file, essentially just replaces it with a new empty one.
-  // defaults to false
-  removeCommand.Flags().BoolP("clear", "c", false, "Clear the entire config.")
+	// --clear | -c | To clear the entire config file, essentially just replaces it with a new empty one.
+	// defaults to false
+	removeCommand.Flags().BoolP("clear", "c", false, "Clear the entire config.")
 }
 
+// the remove command to remove repo from config.
+var removeCommand = &cobra.Command{
 
-var removeCommand = &cobra.Command {
+	Use:   "remove",
+	Short: "Removes a repository locally.",
+	Long:  "Remove a git repository or stop managing it. Only works locally. Would not delete the remote repository.",
+	Run: func(cmd *cobra.Command, args []string) {
 
-  Use: "remove",
-  Short: "Removes a repository locally.",
-  Long: "Remove a git repository or stop managing it. Only works locally. Would not delete the remote repository.",
-  Run: func(cmd *cobra.Command, args []string) {
+		// checking -c | --clear flag
+		if clearConfig, _ := cmd.Flags().GetBool("clear"); clearConfig {
 
-    // checking -c | --clear flag
-    if clearConfig, _ := cmd.Flags().GetBool("clear"); clearConfig {
+			// log the action to clear the entire config.
+			fmt.Println("Clearing the entire config.")
+			fmt.Println("Note: This does not affect any cloned folders, or anything other than the config in general.")
 
-      fmt.Println("Clearing the entire config.")
-      fmt.Println("Note: This does not affect any cloned folders, or anything other than the config in general.")
-      
-      reporegistry.ClearConfig()
+			// clear the config.
+			reporegistry.ClearConfig()
 
-      os.Exit(0)
-    }
+			os.Exit(0)
+		}
 
-    fmt.Print("Repository Name: ");
-    
-    reader := bufio.NewReader(os.Stdin)
+		// when no flag was passed:
 
-    repoName, _ := reader.ReadString('\n')
+		// create the reader.
+		reader := bufio.NewReader(os.Stdin)
 
-    repoName = strings.ToLower(repoName)
+		// input the repository name to remove.
+		fmt.Print("Repository Name: ")
+		repoName, _ := reader.ReadString('\n')
+		repoName = strings.ToLower(repoName)
 
-    reporegistry.DeleteRepository(true, strings.TrimSpace(repoName))
-  },
+		// invoke DeleteRepository to actually do the deleting.
+		reporegistry.DeleteRepository(true, strings.TrimSpace(repoName))
+	},
 }
