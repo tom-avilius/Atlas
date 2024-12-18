1876efdfc4bf4c690e3694498c311bd116c44878
tom avilius <tomavilius@tomavilius.in>
2024-12-18 19:56:57 +0530

    Add documentation to filereader.go

--- internal/repoinformer/filereader.go
+++ internal/repoinformer/filereader.go
@@ -1,6 +1,15 @@
+/**
+*
+* @file: filereader.go
+* @description: This file provide functions to read yaml files..
+*
+* @author: tom avilius <tomavilius@tomavilius.in>
+* @license: MIT license
+* @package: Atlas v0.0.1 development
+*
+**/
 
 package repoinformer
-
 
 import (
 	"fmt"
@@ -10,57 +19,59 @@ import (
 	"tomavilius.in/atlas/internal/reporegistry"
 )
 
+/**
+*
+* @func: ReadYaml()
+* @description: readYaml reades the yaml file and unmarshals it to YamlData structure
+* return false if error occurs otherwise returns YamlData
+*
+**/
+func ReadYaml(filepath string) (*reporegistry.YamlData, bool) {
 
+	// handle ~ directory
+	if path, success := reporegistry.HandleHomeDirectory(filepath); success {
 
-// readYaml reades the yaml file and unmarshals it to YamlData structure
-// return false if error occurs otherwise returns YamlData
-func ReadYaml (filepath string) (*reporegistry.YamlData, bool) {
+		filepath = path
+	} else {
 
-  // handle ~ directory
-  if path, success := reporegistry.HandleHomeDirectory(filepath); success {
-    
-    filepath = path
-  } else {
+		// unsuccessful
+		return nil, false
+	}
 
-    // unsuccessful
-    return nil, false
-  }
+	// if the file does not exist then exist the function
+	if _, err := os.Stat(filepath); os.IsNotExist(err) {
 
-  // if the file does not exist then exist the function
-  if _, err := os.Stat(filepath); os.IsNotExist(err) {
+		fmt.Println("Provided file does not exist.")
+		fmt.Println("Error log:")
+		fmt.Println(err)
+		return nil, false
+	}
 
-    fmt.Println("Provided file does not exist.")
-    fmt.Println("Error log:")
-    fmt.Println(err)
-    return nil, false
-  }
+	// read the yaml file
+	content, err := os.ReadFile(filepath)
+	// PERF: Use go's way of writing if statements.
+	if err != nil {
 
-  // read the yaml file
-  content, err := os.ReadFile(filepath)
-  // Log if an error occurs
-  if err != nil {
+		fmt.Println("Error occured while reading file.")
+		fmt.Println("Error log:")
+		fmt.Println(err)
+		return nil, false
+	}
 
-    fmt.Println("Error occured while reading file.")
-    fmt.Println("Error log:")
-    fmt.Println(err)
-    return nil, false
-  }
+	// empty data structure to hold yaml data
+	var data reporegistry.YamlData
 
-  // empty data structure to hold yaml data
-  var data reporegistry.YamlData
+	// unmarshal the yaml data
+	err = yaml.Unmarshal(content, &data)
+	// PERF: Use go's way of writing if statements.
+	if err != nil {
 
-  // unmarshal the yaml data
-  err = yaml.Unmarshal(content, &data)
-  // log if an error occurs
-  if err != nil {
+		fmt.Println("Error occured while unmarshaling the yaml data.")
+		fmt.Println("Error log:")
+		fmt.Println(err)
+		return nil, false
+	}
 
-    fmt.Println("Error occured while unmarshaling the yaml data.")
-    fmt.Println("Error log:")
-    fmt.Println(err)
-    return nil, false
-  }
-
-  // safely exit the function
-  return &data, true
+	// safely exit the function
+	return &data, true
 }
-
