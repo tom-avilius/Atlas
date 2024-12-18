8e6b84b9977df1e4e408dceccd006d892018a334
tom avilius <tomavilius@tomavilius.in>
2024-12-18 19:35:31 +0530

    Add documentation to systemd.go

--- internal/atlasinstall/systemd.go
+++ internal/atlasinstall/systemd.go
@@ -1,3 +1,14 @@
+/**
+*
+* @file: systemd.go
+* @description: This file creates a systemd service file for atlas.
+*
+* @author: tom avilius <tomavilius@tomavilius.in>
+* @license: MIT license
+* @package: Atlas v0.0.1 development
+*
+**/
+
 package atlasinstall
 
 import (
@@ -5,19 +16,28 @@ import (
 	"os"
 )
 
+/**
+*
+* @func: generateServiceFile()
+* @description: function to generate atlas service file to run the start command on login.
+*
+**/
 // TODO: Move all systemd file functions to a single file..
-func generateServiceFile () bool {
+func generateServiceFile() bool {
 
-  execPath, err := os.Executable();
-  if err != nil {
+	// getting the executable path directory for atals binary.
+	// PERF: Use go's way of writing if statements.
+	execPath, err := os.Executable()
+	if err != nil {
 
-    fmt.Println("Error occured while getting executable path.")
-    fmt.Println("Error Log:")
-    fmt.Println(err)
-    return false
-  }
+		fmt.Println("Error occured while getting executable path.")
+		fmt.Println("Error Log:")
+		fmt.Println(err)
+		return false
+	}
 
-  fileContent := fmt.Sprintf(`[Unit]
+	// service file content for atlas.
+	fileContent := fmt.Sprintf(`[Unit]
   Description=Atlas Daemon
   After=network.target
 
@@ -29,27 +49,34 @@ func generateServiceFile () bool {
   WantedBy=multi-user.target
   `, execPath)
 
-  servicePath := "/etc/systemd/system/atlas.service"
-  file, err := os.Create(servicePath)
-  if err != nil {
-    
-    fmt.Println("Error occured while creating atlas service path.")
-    fmt.Println("Error Log:")
-    fmt.Println(err)
-    return false
-  }
-  defer file.Close()
+	// path where the service file is to be place.
+	// TEST: Should this be somewhere else in a different file... or have global scope?
+	servicePath := "/etc/systemd/system/atlas.service"
 
-  _, err = file.WriteString(fileContent)
-  if err != nil {
+	// creating the systemd service file at the service path.
+	// PERF: Use go's way of writing if statements.
+	file, err := os.Create(servicePath)
+	if err != nil {
 
-    fmt.Println("Error occured while writing to atlas service file.")
-    fmt.Println("Error Log:")
-    fmt.Println(err)
-    return false
-  }
+		fmt.Println("Error occured while creating atlas service path.")
+		fmt.Println("Error Log:")
+		fmt.Println(err)
+		return false
+	}
+	defer file.Close()
 
-  // TODO: Maybe show a notification
-  fmt.Println("Atlas service file created.")
-  return true
+	// writing to the service file the service content.
+	// PERF: Use go's way of writing if statements.
+	_, err = file.WriteString(fileContent)
+	if err != nil {
+
+		fmt.Println("Error occured while writing to atlas service file.")
+		fmt.Println("Error Log:")
+		fmt.Println(err)
+		return false
+	}
+
+	// TODO: Maybe show a notification
+	fmt.Println("Atlas service file created.")
+	return true
 }
