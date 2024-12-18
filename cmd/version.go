dc8b5185a6daf3ef5abbce3b41ff9251dc9a1928
tom avilius <tomavilius@tomavilius.in>
2024-12-18 19:14:56 +0530

    Add comments to version.go

--- cmd/version.go
+++ cmd/version.go
@@ -1,3 +1,14 @@
+/**
+*
+* @file: version.go
+* @description: This file contains the version command to display the atlas version.
+*
+* @author: tom avilius <tomavilius@tomavilius.in>
+* @license: MIT license
+* @package: Atlas v0.0.1 development
+*
+**/
+
 package cmd
 
 import (
@@ -6,21 +17,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func init() {
 
-func init () {
-
-  rootCommand.AddCommand(versionCommand)
+	rootCommand.AddCommand(versionCommand)
 }
 
-
+// versionCommand to display the version of atlas.
 var versionCommand = &cobra.Command{
 
-  Use: "version",
-  Short: "Displays the version.",
-  Long: "Displays the installed version of the atlas project.",
-  Run: func (cmd *cobra.Command, args []string) {
-    
-    fmt.Println("v0.0.1 - development")
-  },
-}
+	Use:   "version",
+	Short: "Displays the version.",
+	Long:  "Displays the installed version of the atlas project.",
+	Run: func(cmd *cobra.Command, args []string) {
 
+		// print the current version
+		// HACK: Ain't there a better way to do this shit, than just hardcoding it?
+		fmt.Println("v0.0.1 - development")
+	},
+}
