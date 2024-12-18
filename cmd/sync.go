e682af08eaf85547fd41c06372556fd565f963bc
tom avilius <tomavilius@tomavilius.in>
2024-12-18 19:12:23 +0530

    Add comments to sync.go

--- cmd/sync.go
+++ cmd/sync.go
@@ -1,3 +1,14 @@
+/**
+*
+* @file: sync.go
+* @description: This file contains the sync command to push to git repository if any change arises.
+*
+* @author: tom avilius <tomavilius@tomavilius.in>
+* @license: MIT license
+* @package: Atlas v0.0.1 development
+*
+**/
+
 package cmd
 
 import (
@@ -5,25 +16,26 @@ import (
 	"tomavilius.in/atlas/internal/reposync"
 )
 
+func init() {
 
-
-func init () {
-
-  rootCommand.AddCommand(syncCommand)
+	rootCommand.AddCommand(syncCommand)
 }
 
+// TODO: Implement the push and commit function in this command.
+// TEST: Does the add functionality even work?
 
-var syncCommand = &cobra.Command {
+// sync command to push local changes to the remote repository.
+var syncCommand = &cobra.Command{
 
-  Use: "sync",
-  Short: "Sync the local folders with github repository.",
-  Long: "Performs a git push if any local changes arise for all the repositories.",
-  Run: func(cmd *cobra.Command, args []string) {
-  
-    // writing path data
-    // TODO: Inside the function have some method to check for 
-    // changes and only then write to the file.
-    reposync.WritePathData()
-  },
+	Use:   "sync",
+	Short: "Sync the local folders with github repository.",
+	Long:  "Performs a git push if any local changes arise for all the repositories.",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		// writing path data
+		// TODO: Inside the function have some method to check for
+		// changes and only then write to the file.
+		// TEST: I guess the above is implemented but ensure that it works.
+		reposync.WritePathData()
+	},
 }
-
