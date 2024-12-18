aa2a7fbe9679a5ef9f0d113e4b4ae339ad97a1d2
tom avilius <tomavilius@tomavilius.in>
2024-12-18 19:09:00 +0530

    Add comments to add.go

--- cmd/start.go
+++ cmd/start.go
@@ -1,3 +1,14 @@
+/**
+*
+* @file: start.go
+* @description: This file contains the start command to start the necessary start up components for the atlas daemon.
+*
+* @author: tom avilius <tomavilius@tomavilius.in>
+* @license: MIT license
+* @package: Atlas v0.0.1 development
+*
+**/
+
 package cmd
 
 import (
@@ -6,20 +17,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func init() {
 
-func init () {
-
-  rootCommand.AddCommand(startCommand)
+	rootCommand.AddCommand(startCommand)
 }
 
+// TODO: Implement the start command.
 
-var startCommand = &cobra.Command {
+// start command to start the necessary atlas components
+var startCommand = &cobra.Command{
 
-  Use: "start",
-  Short: "Start the atlas daemon.",
-  Long: "Start atlas service along with necessary components.",
-  Run: func(cmd *cobra.Command, args []string) {
+	Use:   "start",
+	Short: "Start the atlas daemon.",
+	Long:  "Start atlas service along with necessary components.",
+	Run: func(cmd *cobra.Command, args []string) {
 
-    fmt.Println("hehe")
-  },
+		fmt.Println("hehe")
+	},
 }
