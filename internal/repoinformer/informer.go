9337a9619a668117276d303cdea65b55170349ca
tom avilius <tomavilius@tomavilius.in>
2024-12-18 20:00:49 +0530

    Add documentation to informer.go

--- internal/repoinformer/informer.go
+++ internal/repoinformer/informer.go
@@ -1,15 +1,36 @@
+/**
+*
+* @file: informer.go
+* @description: This file exposes functions that read the config and print it.
+*
+* @author: tom avilius <tomavilius@tomavilius.in>
+* @license: MIT license
+* @package: Atlas v0.0.1 development
+*
+**/
 
 package repoinformer
 
+/**
+*
+* @func: DetailedRepositoryView()
+* @description: DetailedRepositoryView shows formatted details from atlas config
+* NOTE: It prints the entire config.
+*
+**/
+func DetailedRepositoryView() bool {
 
-// DetailedRepositoryView shows formatted details from atlas config
-func DetailedRepositoryView () bool {
-
-  return listRepositories()
+	return listRepositories()
 }
 
+/**
+*
+* @func: ListRepositories()
+* @description: ListRepositories() shows details from atlas config.
+* NOTE: It prints only the repo names from atlas config.
+*
+**/
+func ListRepositories() bool {
 
-func ListRepositories () bool {
-
-  return listOnlyRepositories()
+	return listOnlyRepositories()
 }
