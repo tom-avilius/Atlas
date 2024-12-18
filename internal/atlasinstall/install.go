c21d04c3c0ece353cc0ab1ba44c4015503f4c71f
tom avilius <tomavilius@tomavilius.in>
2024-12-18 19:24:27 +0530

    Add documentation to install.go

--- internal/atlasinstall/install.go
+++ internal/atlasinstall/install.go
@@ -1,3 +1,14 @@
+/**
+*
+* @file: install.go
+* @description: This file brings together code to create systemd service file, enable service and reload systemctl and to start the service.
+*
+* @author: tom avilius <tomavilius@tomavilius.in>
+* @license: MIT license
+* @package: Atlas v0.0.1 development
+*
+**/
+
 package atlasinstall
 
 import (
@@ -5,40 +16,48 @@ import (
 	"os/exec"
 )
 
+/**
+*
+* @func: InstallAtlas()
+* @description: Brings together code to create, reload, start and enable the atlas service.
+*
+**/
+func InstallAtlas() bool {
 
+	// creating the systemd service file.
+	if !generateServiceFile() {
 
-func InstallAtlas () bool {
+		return false
+	}
 
-  if !generateServiceFile() {
+	// reloading the systemd daemon.
+	if err := exec.Command("systemctl", "daemon-reload").Run(); err != nil {
 
-    return false
-  }
+		fmt.Println("Error while running command: systemctl daemon-reload.")
+		fmt.Println("Error Log:")
+		fmt.Println(err)
+		return false
+	}
 
-  if err := exec.Command("systemctl", "daemon-reload").Run(); err != nil {
+	// enabling the atlas service.
+	if err := exec.Command("systemctl", "enable", "atlas").Run(); err != nil {
 
-    fmt.Println("Error while running command: systemctl daemon-reload.")
-    fmt.Println("Error Log:")
-    fmt.Println(err)
-    return false
-  }
+		fmt.Println("Error while running systemctl enable atlas")
+		fmt.Println("Error Log:")
+		fmt.Println(err)
+		return false
+	}
 
-  if err := exec.Command("systemctl", "enable", "atlas").Run(); err != nil {
+	// starting the atlas service.
+	if err := exec.Command("systemctl", "start", "atlas").Run(); err != nil {
 
-    fmt.Println("Error while running systemctl enable atlas")
-    fmt.Println("Error Log:")
-    fmt.Println(err)
-    return false
-  }
+		fmt.Println("Error while running systemctl start atlas.")
+		fmt.Println("Error Log:")
+		fmt.Println(err)
+		return false
+	}
 
-  if err := exec.Command("systemctl", "start", "atlas").Run(); err != nil {
-
-    fmt.Println("Error while running systemctl start atlas.")
-    fmt.Println("Error Log:")
-    fmt.Println(err)
-    return false
-  }
-
-  // TODO: Maybe include a notification
-  fmt.Println("Systemd service created and started successfully.")
-  return true
+	// TODO: Maybe include a notification
+	fmt.Println("Systemd service created and started successfully.")
+	return true
 }
